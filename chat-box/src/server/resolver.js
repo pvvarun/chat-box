8ea49f3e0b82dc8cb38a210968ac8ac611bedea9
pvvarun <varun.mishra@successive.tech>
2019-04-09 15:34:33 +0530

    feature: completed basic structure of backhand(#chat)

--- chat-box/src/server/resolver.js
+++ chat-box/src/server/resolver.js
@@ -1,39 +1,153 @@
-const users = [
+import{ GraphQLScalarType} from 'graphql';
+import { PubSub } from 'apollo-server';
+
+const pubsub = new PubSub();
+
+let users = [
     {
         id: "user0",
         name: "VARUN",
-        email: "varun@gmail.com",
-        friend: ["YOGESH"],
-        chat: []
+        email: "varun@successive.tech",
+        friends: ["user1"],
     },
     {
         id: "user1",
         name: "YOGESH",
-        email: "yogesh@gmail.com",
-        friend: ["VARUN"],
-        chat: []
+        email: "yogesh@successive.tech",
+        friends: ["user0"],
     }
 ];
+
+let messages = [];
+
 let userCount = users.length;
 
 const resolvers = {
+
+    Date: new GraphQLScalarType({
+        name: 'Date',
+        description: 'Date custom scalar type',
+        parseValue(value) {
+            return new Date(value); // value from the client
+        },
+        serialize(value) {
+            return value; // value sent to the client
+        },
+        parseLiteral(ast) {
+            if (ast.kind === Kind.INT) {
+            return parseInt(ast.value, 10); // ast value is always in string format
+            }
+            return null;
+        },
+    }),
+
+    Subscription: {
+        userAdded: {
+          // Additional event labels can be passed to asyncIterator creation
+            subscribe: () => pubsub.asyncIterator([USER_ADDED]),
+        },
+    },
+
     Query: {
-        users: () => users
+        user_friends: (root, { id }, context) => {
+            // console.log('------id of user is ------', id);
+            let friendsId, friendsName = [];
+            users.forEach(element => {
+                if (element.id === id) {
+                    friendsId = element.friends;
+                }
+                else{
+                    friendsName.push(element.name);
+                }
+            }
+            );
+            // console.log('-----------------------', friendsName, friendsId);
+            friendsName = (friendsId) ? friendsName : [];
+            // console.log('friends', friendsName, friendsId);
+            return ({friend_Name_List: friendsName, friend_Id_List: friendsId});
+        },
+        chatHandle: ( root, { to, from }, context) => {
+            let arrayOfChats = [];
+            // console.log('users--', users);
+            // messages.forEach(element => {
+            //     if((element.to === to && element.from === from)|| (element.to === from && element.from === to)) {
+            //         array.push({
+            //             chat: {...element.messagesBetweenFriends},
+            //             sentBy: element.from,
+            //         });
+            //     }
+            // })
+            //     console.log("-----array is-----", array);
+            //     array.forEach(value => {
+            //         arrayOfChats.push({message: value.chat.message,  sendAt: value.chat.sendAt, from: value.sentBy})
+            //     })
+            //     console.log('array of chats is--------', arrayOfChats);
+            //     return arrayOfChats;
+            // }
+            let chatsByUser1 = messages.find(element => element.to === to && element.from === from);
+            let chatsByUser2 = messages.find(element => element.to === from && element.from === to);
+            if(chatsByUser1) {
+                chatsByUser1.messagesBetweenFriends.forEach(element => 
+                    {element.from = chatsByUser1.from
+                arrayOfChats.push(element);
+                    })
+                // console.log('--------arrayOfChats---------', arrayOfChats);
+            }
+            if(chatsByUser2){ 
+                chatsByUser2.messagesBetweenFriends.forEach(element => 
+                    {element.from = chatsByUser2.from
+                    arrayOfChats.push(element);                    
+                    })
+                // console.log('--------arrayOfChats---------', arrayOfChats);            
+            }
+            // console.log('--------modified array1---------', chatsByUser1);
+            // console.log('--------modified array2---------', chatsByUser2);
+            console.log('--------arrayOfChats1---------', arrayOfChats);
+            arrayOfChats.sort((a, b) => {
+                return new Date(a.sendAt) - new Date(b.sendAt);
+                });
+            console.log('--------arrayOfChats2---------', arrayOfChats);
+            return arrayOfChats;
+        }
     },
     Mutation: {
-        addUser: (parent, args) => {
-            const user = {
+        addUser: (root, { name, email }, context) => {
+            // console.log('name--', name);
+            // console.log('email--', email);
+            const newUser = {
                 id: `user${userCount++}`,
-                name: args.name,
-                email: args.email,
-                friend: []
+                name: name,
+                email: email,
+                friends: [],
             };
             users.forEach(element => {
-                element.friend.push(args.name);
-                user.friend.push(element.name);
+                element.friends.push(newUser.id);
+                newUser.friends.push(element.id);
             });
-            users.push(user);
-            return user;
+            users.push(newUser);
+            // console.log('-----new user details are------', newUser);
+            // console.log('-----new user unique id is------', newUser.id);
+            pubsub.publish(USER_ADDED, { userAdded: { name, email } });
+            return {id: newUser.id};
+        },
+        chatWithFriend: (parent, { to, from, msg }, context) => {
+            let flag = true;
+            let date = new Date();
+            console.log('----date-----', date);
+            // console.log('-----from------', from);
+            // console.log('-----message------', msg);
+            messages.forEach(element => {
+                if(element.to === to && element.from === from) {
+                    element.messagesBetweenFriends.push({message: msg, sendAt: date});
+                    flag = false;
+                    // console.log('---------HURRAY--------');
+                }
+            });
+            if (flag){
+                messages.push({to: to, from: from, messagesBetweenFriends: [{message: msg, sendAt: date}]})
+            }
+            console.log('---MESSAGES----', messages);
+            return {msg,  Date: date};
         }
     }
 };
--- chat-box/src/server/schema.js
+++ chat-box/src/server/schema.js
@@ -2,25 +2,55 @@
 import { gql } from 'apollo-server';
 
 const typedef = gql`
+
+scalar Date
+
+type object {
+    to: String
+    from: String
+    msg: String
+}
+
 type user {
     id: String
     name: String
     email: String
-    friend: [String]
-    chat: [object]
+    friends: [String]
 }
 
-type object {
-    to: String,
-    from: String,
-    message: String,
+type Friends {
+    friend_Name_List: [String]
+    friend_Id_List: [String]
+}
+
+type chats {
+    message: String
+    sendAt: Date
+    from: String
+}
+
+type ObjectOfAddFriendMutation{
+    id: String
+}
+
+type ObjectOfChatWithFriendMutation{
+    msg: String
+    sendAt: Date
 }
 
 type Query {
-    users: [user]
+    user_friends(id: String!): Friends
+    chatHandle(to: String!, from: String!): [chats]
 }
+
 type Mutation {
-    addUser(name: String!, email: String!): user
+    addUser(name: String!, email: String!): ObjectOfAddFriendMutation
+    chatWithFriend(to: String!, from: String!, msg: String!): ObjectOfChatWithFriendMutation
 }
+
+type Subscription {
+    userAdded: Friends
+}
+
 `;
 export default typedef;
